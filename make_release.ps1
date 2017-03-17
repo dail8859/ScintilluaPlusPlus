5a98fefbd5e2b74702e6614e62e72a46997df228
dail8859 <dail8859@yahoo.com>
2017-03-17 11:25:14 -0400

    Add release script

---
+++ make_release.ps1
@@ -0,0 +1,17 @@
+Write-Host "Creating Scintillua++ Release"
+
+Remove-Item .\Scintillua++.zip -ErrorAction SilentlyContinue
+Remove-Item .\release\* -Force -Recurse -ErrorAction SilentlyContinue
+
+New-Item -ItemType directory -Path .\release\config\Scintillua++
+
+Copy-Item .\bin\Release_Win32\Scintillua++.dll .\release
+Copy-Item .\bin\Release_Win32\LexLPeg.dll .\release\config\Scintillua++
+
+Copy-Item .\extra\Scintillua++.ini .\release\config
+Copy-Item .\ext\Scintillua\lexers\* .\release\config\Scintillua++ -recurse
+Copy-Item .\extra\npp.lua .\release\config\Scintillua++\themes
+
+Compress-Archive -Path .\release\* -DestinationPath .\Scintillua++.zip
+
+Remove-Item .\release\ -Force -Recurse
