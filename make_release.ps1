9f8ba4899af3bddb0be1e597e10860ab0f3a6579
dail8859 <dail8859@yahoo.com>
2017-03-22 18:07:51 -0400

    Update release script

--- make_release.ps1
+++ make_release.ps1
@@ -1,17 +1,24 @@
-Write-Host "Creating Scintillua++ Release"
+Write-Host "Creating Scintillua++ Releases"
 
-Remove-Item .\Scintillua++.zip -ErrorAction SilentlyContinue
-Remove-Item .\release\* -Force -Recurse -ErrorAction SilentlyContinue
+function make_release($build, $postfix)
+{
+	Remove-Item .\ScintilluaPlusPlus$postfix.zip -ErrorAction SilentlyContinue
+	Remove-Item .\release\* -Force -Recurse -ErrorAction SilentlyContinue
 
-New-Item -ItemType directory -Path .\release\config\Scintillua++
+	New-Item -ItemType directory -Path .\release\config\Scintillua++
 
-Copy-Item .\bin\Release_Win32\Scintillua++.dll .\release
-Copy-Item .\bin\Release_Win32\LexLPeg.dll .\release\config\Scintillua++
+	Copy-Item .\bin\Release_$build\Scintillua++$postfix.dll .\release
+	#Copy-Item .\bin\Release_$build\LexLPeg$postfix.dll .\release\config\Scintillua++
+	Copy-Item .\bin\Release_$build\LexLPeg.dll .\release\config\Scintillua++
 
-Copy-Item .\extra\Scintillua++.ini .\release\config
-Copy-Item .\ext\Scintillua\lexers\* .\release\config\Scintillua++ -recurse
-Copy-Item .\extra\npp.lua .\release\config\Scintillua++\themes
+	Copy-Item .\extra\Scintillua++.ini .\release\config
+	Copy-Item .\ext\Scintillua\lexers\* .\release\config\Scintillua++ -recurse
+	Copy-Item .\extra\npp.lua .\release\config\Scintillua++\themes
 
-Compress-Archive -Path .\release\* -DestinationPath .\Scintillua++.zip
+	Compress-Archive -Path .\release\* -DestinationPath .\ScintilluaPlusPlus$postfix.zip
 
-Remove-Item .\release\ -Force -Recurse
+	Remove-Item .\release\ -Force -Recurse
+}
+
+make_release "Win32" ""
+make_release "x64" "_64"
